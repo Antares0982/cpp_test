327bd4cec0e790c0a1ed518c5ebfd7a6c88f8825
antares <MG20210005@smail.nju.edu.cn>
2021-05-03 19:25:23 +0800

    feat: wtf i didn't remember

--- main.cpp
+++ main.cpp
@@ -3,15 +3,44 @@
 #include<vector>
 using namespace std;
 
-int gcd(int a, int b)
+struct Node 
 {
-	return 0;
+	int Val;
+	Node* Next;
+};
+
+Node* createNodesWithArray(int *a, int length) 
+{
+	Node* head = new Node;
+	head->Val = *a;
+	Node* p = head;
+	for (int i = 1; i < length; i++) 
+	{
+		p->Next = new Node;
+		p = p->Next;
+		p->Val = a[i];
+	}
+	return head;
+}
+
+void printNodes(Node* head) 
+{
+	Node* nodes = head;
+	if (!nodes) { return; }
+	cout << nodes->Val;
+	while (nodes->Next)
+	{
+		cout << '\t';
+		nodes = nodes->Next;
+		cout << nodes->Val;
+	}
+	return;
 }
 
 int main()
 {
-	int a = 5;int b = 2;
-	string c("fegv");
-	vector< int > ivec(10);
-	double* p;
+	int a[4] = { 1,2,3,4 };
+	int* aa = a;
+	Node* nodes = createNodesWithArray(aa, 4);
+	printNodes(nodes);
 }
